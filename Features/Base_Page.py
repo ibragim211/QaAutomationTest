2f3705a18580b28f610fa6fcf67966477f88c3be
Mirsahib <mirisahib97@gmail.com>
2021-05-18 15:39:46 +0400

    18.05.2021 (variables_and_until)

--- Features/Base_Page.py
+++ Features/Base_Page.py
@@ -1,23 +1,20 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
-
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 class Page:
 
     def __init__(self, driver):
         self.driver = driver
+        self.mail = "mirisahib97@gmail.com"
+        self.password = "0558098899"
+        self.link = "https://staging.tut.az"
 
     def find_element(self, *locator):
         return self.driver.find_element(by=locator[0],
-                                       value=locator[1])
-
-    # def find_element(self, *locator):
-    #     by = locator[0]
-    #     value = locator[1]
-    #     WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((by, value)))
-    #     return self.driver.find_element(by, value)
+                                        value=locator[1])
 
     # def find_element_and_click(self, locator, timeout):
     #    try:
@@ -31,27 +28,26 @@ class Page:
     def click_on_element(self, *locator):
         by = locator[0]
         value = locator[1]
-        WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((by, value)))
+        WebDriverWait(self.driver, 10000).until(EC.element_to_be_clickable((by, value)))
+
         element = self.driver.find_element(by, value)
         element.click()
 
     def input(self, text, *locator):
-        element = self.driver.find_element(by=locator[0],
-                                           value=locator[1])
+        by = locator[0]
+        value = locator[1]
+        WebDriverWait(self.driver, 10000).until(EC.presence_of_element_located((by, value)))
+
+        element = self.driver.find_element(by, value)
         element.clear()
         element.send_keys(text)
 
-    # def input(self, text, *locator):
-    #     by = locator[0]
-    #     value = locator[1]
-    #     WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable((by, value)))
-    #     element = self.driver.find_element(by, value)
-    #     element.clear()
-    #     element.send_keys(text)
-
     def upload(self, text, *locator):
-        element = self.driver.find_element(by=locator[0],
-                                           value=locator[1])
+        by = locator[0]
+        value = locator[1]
+        WebDriverWait(self.driver, 10000).until(EC.presence_of_element_located((by, value)))
+
+        element = self.driver.find_element(by, value)
         element.send_keys(text)
 
     def move_to_element(self, *locator):
@@ -59,3 +55,8 @@ class Page:
                                            value=locator[1])
         element.actions = ActionChains(self.driver)
         element.actions.move_to_element(element).perform()
+
+    def wait(self, *locator):
+        by = locator[0]
+        value = locator[1]
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((by, value)))
--- Features/POM/Sign_In_Page.py
+++ Features/POM/Sign_In_Page.py
@@ -11,47 +11,42 @@ class LoginPage(Page):
     _fillEmailField = By.ID, 'j-u-login-email'
     _fillPasswordField = By.ID, 'j-u-login-pass'
     _clickOnDaxilOlButton = By.XPATH, '//button[@class="btn btn-success j-submit"]'
-    _logoVerify = By.XPATH, '//img[@alt="Tut.az"]'
+    _logoVerify = By.XPATH, '//div[@class="products-block_title"]'
     _popUpMessage = By.XPATH, '//*[text()="Forma doldurularkən bu səhvlər baş verdi:"]'
     _warningForInvalidData = By.XPATH, '//*[text()="E-mail və ya şifrə səhv qeyd olunub"]'
 
     def open_tut_az(self):
-        self.driver.get('https://staging.tut.az/')
+        self.driver.get(self.link)
 
     def close_pop_up(self):
         self.click_on_element(*self._clickOnCloseButton)
 
     def giris_button_click(self):
-        sleep(1)
         self.click_on_element(*self._clickOnGirisButton)
 
-    def input_email(self, email_data: str):
-        self.input(email_data, *self._fillEmailField)
+    def input_email(self):
+        self.input(self.mail, *self._fillEmailField)
 
     def inputWrongEmail(self, wrong_email_data):
         self.input(wrong_email_data, *self._fillEmailField)
 
-    def input_password(self, password_data: str):
-        self.input(password_data, *self._fillPasswordField)
+    def input_password(self):
+        self.input(self.password, *self._fillPasswordField)
 
     def inputWrongPassword(self, wrong_password_data):
         self.input(wrong_password_data, *self._fillPasswordField)
 
     def daxil_ol_click(self):
-        sleep(1)
         self.click_on_element(*self._clickOnDaxilOlButton)
 
     def logo_verify(self):
-        sleep(1)
         status = self.find_element(*self._logoVerify).is_displayed()
         assert status is True
 
     def pop_up_visible(self):
-        sleep(1)
         status = self.find_element(*self._popUpMessage).is_displayed()
         assert status is True
 
     def warning_for_invalid_data(self):
-        sleep(1)
         status = self.find_element(*self._warningForInvalidData).is_displayed()
         assert status is True
